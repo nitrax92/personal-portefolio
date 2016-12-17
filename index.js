70cb5d2e159ff244dfed14522e5fa26da866a163
Nitrax <Nitrax92@gmail.com>
2016-12-17 14:04:48 +0100

    Templates with Jade, misc styling etc.

--- index.js
+++ index.js
@@ -2,17 +2,31 @@
  * Created by Mello on 17.12.2016.
  */
 
-console.log("Hello, there..");
-
+// Express init. framework
 var express = require('express');
+
+// Express setup
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
+// Where root (server) is located. Needed when including stylesheets and scripts in jade files (template language)
 app.use(express.static(__dirname + '/public'));
 
+app.set('views', __dirname + '/views');
+// Template engine
+app.set('view engine', 'jade');
+
+// Index route
+app.get('/', function(req, res, next){
+   res.render('index', {
+      title: "The Front Page",
+      name: "Martin Engen"
+   });
+});
+
 var cool = require('cool-ascii-faces');
-app.get('/', function(request, response){
+app.get('/smile', function(request, response){
    response.send(cool());
 });
 
